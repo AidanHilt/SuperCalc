d4881669b7b3cd51fad42ea24dae349cfa303b4d
Aidan Hilt <aidanhilt2@gmail.com>
2017-10-12 19:57:34 -0400

    10/12/17: Refined superscript behavior. At least one glitch remains, when an item is deleted in an exponent, and then a replacement is added, the last numbers gets bumped off. Suspect it has to do with items in the backspace() method somehow changing the value of the superScript boolean. Might investigate tonight.

--- app/src/main/java/com/example/aidanthegreat/supercalc/MainCalculatorv2.java
+++ app/src/main/java/com/example/aidanthegreat/supercalc/MainCalculatorv2.java
@@ -1,13 +1,17 @@
 package com.example.aidanthegreat.supercalc;
 
+import android.content.Context;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatEditText;
 import android.text.Html;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.RelativeSizeSpan;
 import android.text.style.SuperscriptSpan;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,29 +21,46 @@ import java.util.Arrays;
 
 public class MainCalculatorv2 extends AppCompatActivity {
 
+    /**List of all possible operator symbols*/
     private static String[] operatorsList = {"+", "-", "/", "*", "sin", "cos", "tan", "(", ")"};
+    /**The ArrayList that stores 2-index arrays with the start and stop location of all superscripts*/
     private static ArrayList<int[]> superscriptArray = new ArrayList<>();
 
-    //TODO Change these to non-static versions
-    private static EditText numbersView;
-    private static Button exponentButton;
-    private static Button decimalButton;
+    public static ArrayList<int[]> getSuperscriptArray(){
+        return superscriptArray;
+    }
 
-    //Array that stores the contents of the TextView
+    /**The view that shows the numbers to be operated on*/
+    //TODO Override the click method so it sets to super- or subscript
+    private EditTextWithUpdate numbersView;
+    /**Button that creates an exponent in the TextView. This is necessary to show the superscript text on the button*/
+    private Button exponentButton;
+    /**Button that adds a decimal point in the TextView. This is necessary so the point is not cut off by the edge of the button*/
+    private Button decimalButton;
+    /**Button that deletes an item of text. This is necessary to add a long click listener*/
+    //TODO Add a long click method
+    private Button deleteButton;
+
+    /**Array that stores the contents of the TextView*/
     private static ArrayList<String> calculationContents = new ArrayList<>();
 
-    //TODO Document the purpose of these fields
+    /**Boolean to track whether or not the input is in superscript mode*/
     private static boolean superScript = false;
-    private static boolean instantDelete = true;
+
+    public static void setSuperScript(boolean b){
+        superScript = b;
+    }
+    /**Boolean to track whether or not the input is in subscript mode*/
     private static boolean subScript = false;
+    /**Boolean to track whether or not the delete button clears the entire screen*/
+    private static boolean instantDelete = true;
 
-    //Storage for numberView contents, necessary because it does not preserve HTML tags when they are added to the view
-    private static String textViewContents = "";
-
-    //Method to build a string from an ArrayList
+    /**Method to build a string from an ArrayList*/
     private static String buildString(ArrayList<String> a){
+        //Initialize a blank string
         String string = "";
 
+        //For every item in the ArrayList, add it to the string, then return it.
         for(String s: a){
             string += s;
         }
@@ -47,42 +68,48 @@ public class MainCalculatorv2 extends AppCompatActivity {
         return string;
     }
 
-    //Builds an ArrayList out of a string, for the purpose of performing mathetmatical operations.
-    //As such, it separates at operators, so that all items are spaced out for a math algorithim
+    /**Builds an ArrayList out of a string, for the purpose of performing mathetmatical operations.
+    As such, it separates at operators, so that all items are spaced out for a math algorithim*/
     private static ArrayList<String> buildOperationList(String s){
         String copyString = s;
         //Replaces the operators with properly spaced versions, so that the string can be split along spaces
         for(String s1: operatorsList){
             copyString = copyString.replace(s1, " " + s1 + " ");
         }
-
-        copyString = copyString.replace(">", "> ");
-        copyString = copyString.replace("</", " </");
-        copyString = copyString.replace("<s", " <s");
-
         String[] stringList = copyString.split(" ");
 
         return new ArrayList<String>(Arrays.asList(stringList));
     }
 
-    //Builds an ArrayList out of a string, for the purpose of character-by-character modification.
-    //As such, it splits along every character
+    /**Builds an ArrayList out of a string, for the purpose of character-by-character modification.
+    As such, it splits along every character*/
     private static ArrayList<String> buildModifierList(String s){
         String[] modified = s.split("");
         return new ArrayList<String>(Arrays.asList(modified));
     }
 
-    //Method to add text to the view
-    private void addTextInView(String addedString){
-        instantDelete = false;
-        int finalPosition = numbersView.getSelectionStart() + 1;
+    void print(int[] a){
+        for(int i : a){
+            System.out.println(i);
+        }
+    }
 
+    /**
+     * Method that adds text to the numbersView
+     * @param addedString: The string to be added to numbersView
+     */
+    private void addTextInView(String addedString){
+        //Disables instant deleting so that all entered code isn't suddenly deleted
+        instantDelete = false;
+        //Position to move the cursor to after
+        int finalPosition = numbersView.getSelectionStart() + 1;
         ArrayList<String> modifier = buildModifierList(numbersView.getText().toString());
 
-        if(inSuperscript(numbersView.getSelectionStart())){
+        if(inSuperscript(numbersView.getSelectionStart()) && superScript){
             modifier.add(numbersView.getSelectionStart() + 1, addedString);
             int newIndex = getRange(numbersView.getSelectionStart());
             superscriptArray.get(newIndex)[1] = superscriptArray.get(newIndex)[1] + 1;
+            print(superscriptArray.get(newIndex));
         }else{
             modifier.add(numbersView.getSelectionStart() + 1, addedString);
         }
@@ -95,7 +122,8 @@ public class MainCalculatorv2 extends AppCompatActivity {
 
         }
 
-    //Method to delete an item from the view
+    //TODO Document this method
+    /**Method to delete an item from the view*/
     private void backspace(){
         String finalModified = "";
         int finalPosition =  numbersView.getSelectionStart() - 1; //numbersView.getSelectionStart();
@@ -108,7 +136,9 @@ public class MainCalculatorv2 extends AppCompatActivity {
             ArrayList<String> modifiedList = buildModifierList(numbersView.getText().toString());
             modifiedList.remove(numbersView.getSelectionStart());
             finalModified = buildString(modifiedList);
+
         }else if(inSuperscript(numbersView.getSelectionStart())){
+
             ArrayList<String> modifiedList = buildModifierList(numbersView.getText().toString());
             modifiedList.remove(numbersView.getSelectionStart());
             finalModified = buildString(modifiedList);
@@ -116,10 +146,12 @@ public class MainCalculatorv2 extends AppCompatActivity {
             clearInvalidSpans();
         }
 
+        decrementSuperscriptSpans(numbersView.getSelectionStart());
         updateNumbersView(finalModified);
         if(numbersView.getText().toString().length() > 0){
             numbersView.setSelection(finalPosition, finalPosition);
         }
+
 
     }
 
@@ -194,13 +226,13 @@ public class MainCalculatorv2 extends AppCompatActivity {
         addTextInView(".");
     }
 
-    //TODO Document this method
     public void exponentClick(View v){
+        //Initialize an int array, with both indices at the current position of the cursor. We then add the array to the ArrayList that stores all the superscript locations, and set superscript to true
         int[] arrayListsAreAssholes = {numbersView.getSelectionStart(), numbersView.getSelectionStart()};
         superscriptArray.add(arrayListsAreAssholes);
         superScript=true;
     }
-    //TODO Document this method
+    //TODO Document and update this method
     public void equalsClick(View v){
         if(superScript){
             superScript = false;
@@ -218,9 +250,33 @@ public class MainCalculatorv2 extends AppCompatActivity {
     }
 
     //-----------Convenience methods------------
-    //TODO Document this method
-    private boolean inSuperscript(int index){
+    private static void incrementSuperscriptSpans(){
+        for(int[] i : superscriptArray){
+            i[0] ++;
+            i[1] ++;
+        }
+    }
+
+    private static void decrementSuperscriptSpans(int index){
+        for(int[] i: superscriptArray){
+            if(i[0] > 0) {
+                if (i[0] >= index) {
+                    i[0]--;
+                    i[1]--;
+                }
+            }
+        }
+    }
+
+    /**
+     * @param index: The index to be tested.
+     * @return Boolean, whether or not the provided index is in any of the currently available superscript ranges
+     */
+    protected static boolean inSuperscript(int index){
+        //By default, this method will return false
         boolean returnValue = false;
+        //Check each array in the list of all superscript locations. If the provided index is larger than the smaller item, and smaller than the larger item, then it is in the range and the
+        //method will break the loop, and return true
         for(int[] item :superscriptArray){
             if(index >= item[0] && index <=item[1]){
                 returnValue = true;
@@ -230,39 +286,72 @@ public class MainCalculatorv2 extends AppCompatActivity {
         return returnValue;
     }
 
-
-    //TODO Document this method
-    private void updateNumbersView(String addedValue){
-        SpannableStringBuilder addText = new SpannableStringBuilder(addedValue);
-        for(int[] item: superscriptArray){
-            addText.setSpan(new SuperscriptSpan(), item[0], item[1], Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-            addText.setSpan(new RelativeSizeSpan(0.75f), item[0], item[1], Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+    protected static boolean inSupersciptAlternate(int index){
+        //By default, this method will return false
+        boolean returnValue = false;
+        //Check each array in the list of all superscript locations. If the provided index is larger than the smaller item, and smaller than the larger item, then it is in the range and the
+        //method will break the loop, and return true
+        for(int[] item :superscriptArray){
+            if(index >= item[0] + 1 && index <=item[1] - 1){
+                returnValue = true;
+                break;
+            }
         }
-        numbersView.setText(addText);
+        return returnValue;
     }
 
-    //TODO Document this method
+    /**
+     * @param index: The index to be checked
+     * @return Index in the superscript array that has provided index in its range. Only call when a value has been found from inSuperscript, otherwise it will return index 0, when nothing is there
+     */
     private int getRange(int index){
+        //By default, this will return 0. As a result, should only be called after the selected index has been confirmed to be in a superscript range
         int returnValue = 0;
+        //For all items in the sueprscript array, we check if the index is larger than the smaller value, and smaller than the larger value. If so, that means that it is a superscript, and we return the value of the current index
         for(int i = 0; i < superscriptArray.size(); i ++){
             if(index >= superscriptArray.get(i)[0] && index <= superscriptArray.get(i)[1]){
                 returnValue = i;
                 break;
-            }
-
-            if(superscriptArray.get(i)[0] > superscriptArray.get(i)[1]){
-                superscriptArray.remove(i);
             }
         }
         return returnValue;
     }
 
     //TODO Document this method
+
+    /**
+     * @param addedValue: The new vlue that numbersView is to display
+     */
+    private void updateNumbersView(String addedValue){
+        //SpannableStringBuilder that will be used to modify the text view
+        SpannableStringBuilder addText = new SpannableStringBuilder(addedValue);
+
+        //For every item in the superscriptArray, create a span that sets the text in that span to superscript, and makes it 75% of the size of the normal text
+        for(int[] item: superscriptArray){
+            addText.setSpan(new SuperscriptSpan(), item[0], item[1], Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+            addText.setSpan(new RelativeSizeSpan(0.75f), item[0], item[1], Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        }
+        //Then we set the text view
+        numbersView.setText(addText);
+    }
+
+    //TODO Document this method
+
+    /**
+     * Purpose: Clears spans from the supersciptArray that have become invalid, because they end before they begin
+     */
     private void clearInvalidSpans(){
+        ArrayList<int[]> remove = new ArrayList<>();
+
+        //Checks every item in superscriptArray. If the first value is larger than the second one, then we remove it from the array
         for(int[] item: superscriptArray){
             if(item[0] > item[1]){
-                superscriptArray.remove(item);
+                remove.add(item);
             }
+        }
+
+        for(int[] item: remove){
+            superscriptArray.remove(item);
         }
     }
 
@@ -289,7 +378,7 @@ public class MainCalculatorv2 extends AppCompatActivity {
         setContentView(R.layout.activity_main_calculatorv2);
 
         //Programmatic representation of the TextView
-        numbersView = (EditText)(findViewById(R.id.numbersView));
+        numbersView = (EditTextWithUpdate)(findViewById(R.id.numbersView));
 
         //Attempts to keep the keyboard from launching when the EditText is clicked
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -311,4 +400,43 @@ public class MainCalculatorv2 extends AppCompatActivity {
         calculationContents.add(getString(R.string.defaultMessage));
     }
 
+}
+
+class EditTextWithUpdate extends AppCompatEditText{
+    public EditTextWithUpdate(Context context){
+        super(context);
+    }
+
+    public EditTextWithUpdate(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    public EditTextWithUpdate(Context context, AttributeSet attrs, int defStyleAttr){
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        if(inSuperscript(this.getSelectionStart())){
+            MainCalculatorv2.setSuperScript(true);
+        }else{
+            MainCalculatorv2.setSuperScript(false);
+        }
+        return super.onTouchEvent(event);
+    }
+
+    private boolean inSuperscript(int index){
+        //By default, this method will return false
+        boolean returnValue = false;
+        //Check each array in the list of all superscript locations. If the provided index is larger than the smaller item, and smaller than the larger item, then it is in the range and the
+        //method will break the loop, and return true
+        for(int[] item : MainCalculatorv2.getSuperscriptArray()){
+            if(index >= item[0] && index <=item[1]){
+                returnValue = true;
+                break;
+            }
+        }
+        return returnValue;
+    }
 }
